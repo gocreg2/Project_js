fdd9b8bd5f05c3039b4a79d8987d2d5ed6ddb65d
Gosha <example@gmail.com>
2021-10-08 14:58:04 +0300

    yes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let category = 'toys';
-console.log(`https://data.com/${category}/5`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genries: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
